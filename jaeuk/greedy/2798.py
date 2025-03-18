bc18bd87e747676f53caad018ee06b2dd808a57a
jaypark <jwpark3639@gmail.com>
2025-03-19 01:07:50 +0900

    remove: remove an unused folder

--- jaeuk/greedy/2798.py
+++
@@ -1,8 +0,0 @@
-from itertools import combinations
-n, m = map(int, input().split())
-cards = list(map(int, input().split()))
-
-
-sum_set = set(sum(combo) for combo in combinations(cards, 3))
-
-print(max([x for x in sum_set if x <= m]))
